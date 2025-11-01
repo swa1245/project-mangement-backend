1c02cde01f876a8a65fe151a4e55c7572e210df7
swa1245 <swarajpatil2123@gmail.com>
2025-11-01 13:04:09 +0530

    folder structure along with dotenv

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config({
+    path: "../.env"
+});
+let myuser = process.env.username;
+console.log("vale" + myuser);
 import express from "express";
 //# sourceMappingURL=index.js.map
